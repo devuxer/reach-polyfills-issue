4b9c2646e78128cee9e99a25ff17a51aaad728f3
Dan M <danspamable@gmail.com>
2019-03-02 21:38:10 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { navigate } from "@reach/router";
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+    return (
+        <>
+            <p>Hello React!</p>
+            <button
+                onClick={() => {
+                    navigateRelativeToRoot("/");
+                }}
+            >
+                Activate Lasers
+            </button>
+        </>
+    );
+}
+
+function navigateRelativeToRoot(relativeUrl) {
+    const baseDirectoryName = "";
+    const url = baseDirectoryName + relativeUrl;
+    navigate(url);
+}
